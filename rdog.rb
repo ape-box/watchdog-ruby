dec3d7fe8019faecbcbd571b465c1887387dbb21
ape-box <alessio.peternelli@gmail.com>
2013-09-17 11:45:55 +0200

    full implementation

--- rdog.rb
+++ rdog.rb
@@ -1,10 +1,29 @@
 require "rb-inotify"
 
 notifier = INotify::Notifier.new
-path = "/home/alessio"
+paths    = ["/home/alessio"]
+unwanted = [".gif", ".php"]
 
-notifier.watch(path, :create) do |event|
-  puts "#{event.name} created in #{path}"
+paths.each do |path|
+    notifier.watch(path, :create, :recursive) do |event|
+      unless event.flags.include?(:isdir)
+        puts "#{event.name} #{event.flags} in #{path} [#{event.absolute_name}]"
+        unwanted.each do |ext|
+            if event.name.end_with?(ext)
+                puts "ALLERT UNWANTED FILE #{event.name}"
+                File.delete(event.absolute_name)
+              end
+          end
+      end
+    end
 end
 
-notifier.run
+status = :running
+trap("SIGINT") { status = :exit }
+while status == :running do
+    if IO.select([notifier.to_io], [], [], 1)
+        notifier.process
+    end
+end
+
+puts "Byez!"
