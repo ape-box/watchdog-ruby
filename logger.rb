758c0310ad5299159c0a5129c0dd02901469b006
ape-box <alessio.peternelli@gmail.com>
2013-09-23 11:46:07 +0200

    added logger, micro refactoring

--- logger.rb
+++ logger.rb
@@ -1,16 +1,16 @@
+
 require "rb-inotify"
 
-notifier = INotify::Notifier.new
 # Beware not adding too many mirectories,
 # i have enabled recursive watching and that's heavy
 # please chek on a virtual machine or somewhere safe
 
-paths    = [
-    "/home/alessio/joomlainstallation1/images",
-    "/home/alessio/joomlainstallation2/images",
-    "/home/alessio/joomlainstallation3/images",
-    "/home/alessio/joomlainstallation4/images",
-]
+# paths    = [
+#     "/home/alessio/joomlainstallation1/images",
+#     "/home/alessio/joomlainstallation2/images",
+#     "/home/alessio/joomlainstallation3/images",
+#     "/home/alessio/joomlainstallation4/images",
+# ]
 
 pwd      = File.dirname(__FILE__)
 unwanted = [".gif", ".php"]         # List of unwanted extensions
@@ -19,12 +19,13 @@ moveto   = "/home/alessio/trash"    # set to nil if you want to delete files ins
 var_run  = "/var/run"               # Change me to a directory you can write, change the same parameter in kill.sh
 #var_run = pwd                     # or uncomment me to save the pid here (remember to update the kill file)
 waitfor  = 60                       # wait for other istance to exit, set to nil to exit immediately
-pid      = 'rdog.pid'
 
+notifier = INotify::Notifier.new
+pid      = 'rdog_logger.pid'
 
 # i prefer having only one instance
 if File.exists?("#{var_run}/#{pid}")
-    puts "\n[#{Time.now}] I am allready runnig!"
+    puts "[#{Time.now}] I am allready runnig!"
     exit if waitfor.nil?
 
     puts "[#{Time.now}] Wainting availability"
@@ -42,36 +43,11 @@ end
 puts "\n[#{Time.now}] I am #{Process.pid}"
 File.open("#{var_run}/#{pid}", 'w') {|f| f.write(Process.pid)}
 
-a = Time.now.to_f * 1000000
-a = a.to_i
 paths.each do |path|
-    # the :recursive option can be safely removed
-    notifier.watch(path, :create, :recursive) do |event|
-        unless event.flags.include?(:isdir)
-            unwanted.each do |ext|
-                if event.name.end_with?(ext)
-                    puts "[#{Time.now}] ALLERT UNWANTED FILE #{event.name}"
-                    if moveto.is_a? String
-                        identifier = Time.now.to_f * 1000000
-                        identifier = identifier.to_i.to_s
-                        while File.exists?("#{moveto}/#{identifier}.danger")
-                            identifier = "#{identifier}-double"
-                        end
-                        puts "[#{Time.now}] identifier id #{identifier}"
-                        File.rename(event.absolute_name, "#{moveto}/#{identifier}.danger")
-                        File.open("#{moveto}/#{identifier}.path", 'w') {|f| f.write(event.absolute_name) }
-                    else
-                        File.delete(event.absolute_name)
-                    end
-                end
-            end
-        end
+    notifier.watch(path, :all_events) do |event|
+        puts "\n#{event.name} #{event.flags} in #{path} [#{event.absolute_name}]"
     end
 end
-
-b = Time.now.to_f * 1000000
-b = b.to_i
-puts "[#{Time.now}] Done adding watchers in #{b - a} micro seconds"
 
 status = :running
 Signal.trap("SIGINT") { status = :exit }
